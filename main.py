018c62cb5287ea28b5f4bfabbe825ffa8be2e854
Jordan Menchen <jmench97@gmail.com>
2020-05-07 12:47:07 -0400

    found the hyperparameters I want

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 #Author: Jordan Menchen
 
 import torch
-import PIL
+import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torchvision
-
 from torch.utils.data.dataloader import DataLoader
 from torchvision import transforms
 
@@ -17,9 +15,9 @@ def main():
   classes = ['pembroke', 'cardigan']
 
   TRANSFORM_IMG = transforms.Compose(
-      [transforms.Resize((32,32)),
-      transforms.ToTensor(),
-      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    [transforms.Resize((32,32)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
   train_data = torchvision.datasets.ImageFolder(root=train_set, transform=TRANSFORM_IMG)
   train_data_loader = DataLoader(train_data, batch_size=4, shuffle=True,  num_workers=2)
@@ -69,7 +67,7 @@ def main():
       running_loss += loss.item()
       if i % 30 == 29:    # print every 2000 mini-batches
         print('[epoch: %d, batch: %3d] loss: %.3f' %
-          (epoch + 1, i + 1, running_loss / 20))
+          (epoch + 1, i + 1, running_loss / 30))
         running_loss = 0.0
 
   print('Finished Training')
@@ -77,34 +75,30 @@ def main():
   correct = 0
   total = 0
   with torch.no_grad():
-      for data in test_data_loader:
-          images, labels = data
-          outputs = net(images)
-          _, predicted = torch.max(outputs.data, 1)
-          total += labels.size(0)
-          correct += (predicted == labels).sum().item()
+    for data in test_data_loader:
+      images, labels = data
+      outputs = net(images)
+      _, predicted = torch.max(outputs.data, 1)
+      total += labels.size(0)
+      correct += (predicted == labels).sum().item()
 
-  print('Accuracy of the network: %d %%' % (
-      100 * correct / total))
+  print('Accuracy of the network: %d %%' % (100 * correct / total))
 
   class_correct = list(0. for i in range(2))
   class_total = list(0. for i in range(2))
   with torch.no_grad():
-      for data in test_data_loader:
-          images, labels = data
-          outputs = net(images)
-          _, predicted = torch.max(outputs, 1)
-          c = (predicted == labels).squeeze()
-          for i in range(4):
-              label = labels[i]
-              class_correct[label] += c[i].item()
-              class_total[label] += 1
-
+    for data in test_data_loader:
+      images, labels = data
+      outputs = net(images)
+      _, predicted = torch.max(outputs, 1)
+      c = (predicted == labels).squeeze()
+      for i in range(4):
+        label = labels[i]
+        class_correct[label] += c[i].item()
+        class_total[label] += 1
 
   for i in range(2):
-        print('Accuracy of %5s : %2d %%' % (
-            classes[i], 100 * class_correct[i] / class_total[i]))
-
+    print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
 
 if __name__ == '__main__':
     main()
