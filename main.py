9e27494e21e1d0825162c240bf3a00abf0dd6499
Jordan Menchen <jmench97@gmail.com>
2020-05-06 21:07:27 -0400

    able to read in files to respective datasets

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 # Author: Jordan Menchen
 
-def main():
-    print('made it here')
+import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
 
-if __name__ == "__main__":
-    main()
+from dataset import read_train_sets
+
+def main():
+    train_set = './data/training_data'
+    test_set = './data/testing_data'
+    image_size = 3
+    classes = ['pembroke', 'cardigan']
+    validation = 0.2
+
+    datasets = read_train_sets(train_set, image_size, classes, validation)
+    print(datasets.train.num_examples())
+
+if __name__ == "__main__": main()
